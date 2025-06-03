a5f1f059e2eb856a3ad6be0b793dba51531641a2
prudviraj <prudvirajvemu316@gmail.com>
2025-06-03 21:52:55 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi ${PERSON2}, How are you"
+echo "${PERSON2}:: Hi $PERSON1, I'm good what about you?"
+echo "$PERSON1:: Yeah, I'm good ${PERSON2}. Whats going on?"
+echo "${PERSON2}:: I'm learning Devops with AWS $PERSON1"
