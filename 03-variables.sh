44b295d15f4e07a13a171ad58aa4d80f958e6f88
prudviraj <prudvirajvemu316@gmail.com>
2025-06-03 21:46:08 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$PERSON!:: Hi ${PERSON2}, How are you"
+echo "$PERSON1:: Hi ${PERSON2}, How are you"
 echo "${PERSON2}:: Hi $PERSON1, I'm good what about you?"
 echo "$PERSON1:: Yeah, I'm good ${PERSON2}. Whats going on?"
 echo "${PERSON2}:: I'm learning Devops with AWS $PERSON1"
