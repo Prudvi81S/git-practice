7fe7adc56794f4f6a7aa1062992249dc204657e2
prudviraj <prudvirajvemu316@gmail.com>
2025-06-03 21:17:45 +0530

    added converstation

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you"
+echo "Suresh:: Hi Ramesh, I'm good what about you?"
+echo "Ramesh:: Yeah, I'm good. Whats goin on?"
+echo "Suresh:: I'm learning Devops with AWS"
